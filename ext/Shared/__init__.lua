0044a1a6a9b141172f4a113430feca05c1aac3cb
Ton Muller <spatieman@gmail.com>
2024-07-20 07:22:07 +0200

    Delete ext directory

--- ext/Shared/__init__.lua
+++
@@ -1,5 +0,0 @@
-Events:Subscribe('Level:LoadingInfo', function(screenInfo)
-	if screenInfo == "Running" or screenInfo == "Blocking on shader creation" or screenInfo == "Loading done" then
-		print("=== Shared stuff loaded ===");
-	end
-end)
