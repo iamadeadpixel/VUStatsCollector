c13a49cb835d81efa724d3a38fbd8218d275bd5a
Ton Muller <spatieman@gmail.com>
2024-10-01 07:56:27 +0200

    Add files via upload

--- ext/Server/Roundover.lua
+++ ext/Server/Roundover.lua
@@ -14,14 +14,56 @@ Events:Subscribe('Server:RoundOver', function(roundTime, winningTeam)
 	s_winningTeam = winningTeam
 	s_endroundtime = os.date('%Y-%m-%d %H:%M:%S')
 
-	if SharedUtils:GetCurrentGameMode() ~= "SquadDeathMatch0" and s_winningTeam == 1 then n_winningTeam = "US"; end
-	if SharedUtils:GetCurrentGameMode() ~= "SquadDeathMatch0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestLarge0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestLarge0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "ConquestSmall0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestSmall0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultSmall0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultSmall0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultLarge0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultLarge0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultSmall1" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "ConquestAssaultSmall1" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "GunMaster0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "GunMaster0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "Domination0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "Domination0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "TankSuperiority0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "TankSuperiority0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "Scavenger0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "Scavenger0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "AirSuperiority0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "AirSuperiority0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "CaptureTheFlag0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "CaptureTheFlag0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "KingOfTheHill0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "KingOfTheHill0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "HeliSuperiority0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "HeliSuperiority0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
 
 	if SharedUtils:GetCurrentGameMode() == "SquadRush0" and s_winningTeam == 1 then n_winningTeam = "Atackers"; end
 	if SharedUtils:GetCurrentGameMode() == "SquadRush0" and s_winningTeam == 2 then n_winningTeam = "Defenders"; end
 
 	if SharedUtils:GetCurrentGameMode() == "RushLarge0" and s_winningTeam == 1 then n_winningTeam = "Atackers"; end
 	if SharedUtils:GetCurrentGameMode() == "RushLarge0" and s_winningTeam == 2 then n_winningTeam = "Defenders"; end
+
+	if SharedUtils:GetCurrentGameMode() == "TeamDeathMatch0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "TeamDeathMatch0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
+
+	if SharedUtils:GetCurrentGameMode() == "TeamDeathMatchC0" and s_winningTeam == 1 then n_winningTeam = "US"; end
+	if SharedUtils:GetCurrentGameMode() == "TeamDeathMatchC0" and s_winningTeam == 2 then n_winningTeam = "RU"; end
 
 	if SharedUtils:GetCurrentGameMode() == "SquadDeathMatch0" and s_winningTeam == 1 then n_winningTeam = "Alpha"; end
 	if SharedUtils:GetCurrentGameMode() == "SquadDeathMatch0" and s_winningTeam == 2 then n_winningTeam = "Beta"; end
@@ -36,3 +78,10 @@ Events:Subscribe('Server:RoundOver', function(roundTime, winningTeam)
 end)
 
 return Roundover()
+
+--[[
+
+
+
+
+]]
