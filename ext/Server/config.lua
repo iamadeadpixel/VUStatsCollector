51bd826b9ea09e2e114cab6df31418b898108f25
Ton Muller <spatieman@gmail.com>
2025-03-30 09:34:09 +0200

    Add files via upload

--- ext/Server/config.lua
+++ ext/Server/config.lua
@@ -9,35 +9,35 @@ end)
 -- Here you can turn on, or off modules.
 Config = {
 	humansonly = false, -- true = Only humans are logged / false = includes bots in logging.
-	consolespam = false, -- if true, the console wil spam all messages (this will increase log file)
+	consolespam = true, -- if true, the console wil spam all messages (this will increase log file)
 	consolespam_chat = false, -- if true, the commands typed to get the stats are not shown in the console
-	consolespam_section1 = false, -- if true, the console wil spam all  tbl_playerdata messages
-	consolespam_section2 = false, -- if true, the console wil spam all tbl_serverstats messages
-	consolespam_section3 = false, -- if true, the console wil spam all tbl_mapstats messages
-	consolespam_section4 = false, -- if true, the console wil spam all tbl_playerstats messages
-	consolespam_section5 = false, -- if true, the console wil spam all Weapon / Vehicle kill messages
-	consolespam_section6 = false, -- if true, the console wil spam all tbl_mcom messages
-	consolespam_killtable = false, -- if true, the console wil spam all SQL messages related to weapon/vehicle stuff
-	consolespam_mcomtable = false, -- if true, the console wil spam all SQL messages related to M-com stuff
-	consolespam_mcomevent = false, -- if true, the console wil spam all M-com event messages
+	consolespam_section1 = true, -- if true, the console wil spam all  tbl_playerdata messages
+	consolespam_section2 = true, -- if true, the console wil spam all tbl_serverstats messages
+	consolespam_section3 = true, -- if true, the console wil spam all tbl_mapstats messages
+	consolespam_section4 = true, -- if true, the console wil spam all tbl_playerstats messages
+	consolespam_section5 = true, -- if true, the console wil spam all Weapon / Vehicle kill messages
+	consolespam_section6 = true, -- if true, the console wil spam all tbl_mcom messages
+	consolespam_killtable = true, -- if true, the console wil spam all SQL messages related to weapon/vehicle stuff
+	consolespam_mcomtable = true, -- if true, the console wil spam all SQL messages related to M-com stuff
+	consolespam_mcomevent = true, -- if true, the console wil spam all M-com event messages
 	consolespam_join = false, -- if true, the console wil spam all Join messages
 	consolespam_leaves = false, -- if true, the console wil spam all leaves messages
 	consolespam_header = false, -- if true, the console wil spam header info on mapload..
 
-	consolespam_common = false, -- if true, the console wil spam header info on common weapons
-	consolespam_roadkill = false, -- if true, the console wil spam 
-	consolespam_shotguns = false, -- if true, the console wil spam 
-	consolespam_handguns = false, -- if true, the console wil spam 
-	consolespam_airvehicles = false, -- if true, the console wil spam 
-	consolespam_landvehicles = false, -- if true, the console wil spam 
-	consolespam_assault_gadgets = false, -- if true, the console wil spam 
-	consolespam_assault_weapons = false, -- if true, the console wil spam 
-	consolespam_engineer_gadgets = false, -- if true, the console wil spam 
-	consolespam_engineer_weapons = false, -- if true, the console wil spam 
-	consolespam_recon_gadgets = false, -- if true, the console wil spam 
-	consolespam_recon_weapons = false, -- if true, the console wil spam 
-	consolespam_support_gadgets = false, -- if true, the console wil spam 
-	consolespam_support_weapons = false -- if true, the console wil spam 
+	consolespam_common = true, -- if true, the console wil spam common weapon kills in the console
+	consolespam_roadkill = true, -- if true, the console wil spam roadkill kills in the console
+	consolespam_shotguns = true, -- if true, the console wil spam Shotgun kills in the console
+	consolespam_handguns = true, -- if true, the console wil spam Handgun kills in the console
+	consolespam_airvehicles = true, -- if true, the console wil spam air vehicle  kills in the console
+	consolespam_landvehicles = true, -- if true, the console wil spam Land vehicles kills in the console
+	consolespam_assault_gadgets = true, -- if true, the console wil spam Assault gadget kills in the console
+	consolespam_assault_weapons = true, -- if true, the console wil spam Assault weapon kills in the console
+	consolespam_engineer_gadgets = true, -- if true, the console wil spam Engineer gadget kills in the console
+	consolespam_engineer_weapons = true, -- if true, the console wil spam Engineer weapon kills in the console
+	consolespam_recon_gadgets = true, -- if true, the console wil spam Recon gadget kills in the console
+	consolespam_recon_weapons = true, -- if true, the console wil spam Recon weapon kills in the console
+	consolespam_support_gadgets = true, -- if true, the console wil spam support gadget kills in the console
+	consolespam_support_weapons = true -- if true, the console wil spam Support weapon kills in the console
 
 
 
